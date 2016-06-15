aefa96a809c676342134f896b41e9b16e835a319
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-06-15 18:45:16 +0300

    bootstrap installed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,11 @@
 class Event < ActiveRecord::Base
 
+  belongs_to :user
+
+  validates :user, presence: true
+
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
-  validates :datetime, presense: true
+  validates :datetime, presence: true
 end
 
