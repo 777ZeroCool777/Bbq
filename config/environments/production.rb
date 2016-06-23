762b223e42a27e58bb67feec74025986bf1f6aa6
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-06-23 14:22:32 +0300

    event-meiler. sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,22 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: 'supermegabbq.herokuapp.com'}
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perrom_deliveries = true
 
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_deliveries = true
+
+  # отправка почты по протоколу SMTP
+  config.action_mailer.delivery_method = :smtp
+
+  # Настройки для Sendgrid
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
