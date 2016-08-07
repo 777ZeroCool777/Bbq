0ba56b3d8360910e0bf06fa71439bb41b9517f50
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-08-07 10:14:47 +0300

    transfer code s.f. in e.s.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,17 @@
 module EventsHelper
+
+  def proverka
+
+    @event.subscriptions.each do |s|
+
+      current_user.subscriptions.each do |s2|
+        if s2 = s2
+          return 1
+        end
+      end
+
+    end
+
+  end
+
 end
