59890a95bd6aa1215e2889849959f52190d425d1
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-08-11 00:01:10 +0300

    refactor mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -51,7 +51,7 @@ class CommentsController < ApplicationController
 
   def notify_subscribers(event, comment)
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email)).uniq.delete(event.user.email)
+    all_emails = (event.subscriptions.map(&:user_email)).uniq.delete("#{event.user.email}")
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
